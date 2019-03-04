6d36af505842dbb21309a49db906febdcb938676
Rushani Shawuti <ruhsane924@gmail.com>
2019-03-04 14:52:17 -0800

    jwt

--- TextSourceAPI/api/controllers/authenticationController.js
+++ TextSourceAPI/api/controllers/authenticationController.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
-const TextSource = require('../models/authModel');
+const User = require('../models/authModel');
+const jwt = require('jsonwebtoken')
 
 module.exports = app => {
 
-    
+    //NEW USER
+    app.post("/sign-up", (req, res) => {
+        const newUser = new User(req.body);
+        newUser.save(function (err) {
+            if (err) console.log(err);
+            // saved!
+            const token = jwt.sign({ _id: user._id }, 'shhhhhhared-secret');
+            console.log(token)
+        });
+    });
+
+
 }
