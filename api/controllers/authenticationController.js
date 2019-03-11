90ca9f2fdcb42e0c37980eb0b16f250348fa2f6f
Rushani Shawuti <ruhsane924@gmail.com>
2019-03-11 06:58:56 -0700

    dry code

--- api/controllers/authenticationController.js
+++ api/controllers/authenticationController.js
@@ -1,55 +1,49 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/authModel');
-const jwt = require('jsonwebtoken')
 
-module.exports = app => {
+module.exports = (app) => {
+  // SIGN UP
+  app.post('/sign-up', (req, res) => {
+    console.log(req.body);
+    const user = new User(req.body);
 
-    //SIGN UP
-    app.post("/sign-up", (req, res) => {
-        console.log(req.body)
-        const user = new User(req.body);
+    user
+      .save()
+      .then((user) => {
+        const token = jwt.sign({ _username: user._username }, process.env.SECRET);
+        res.json({ token });
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.status(400).send({ err });
+      });
+  });
 
-        user
-            .save()
-            .then(user => {
-                const token = jwt.sign({ _username: user._username }, process.env.SECRET);
-                res.json({'token': token});
-            })
-            .catch(err => {
-                console.log(err.message);
-                return res.status(400).send({ err:err });
-            });
-    });
+  // LOG IN
+  app.post('/login', (req, res) => {
+    const { username } = req.body;
+    const { password } = req.body;
 
-    //LOG IN
-    app.post('/login', (req, res) => {
-
-        const username = req.body.username;
-        const password = req.body.password;
-    
-        //find this user name
-        User.findOne({ username }, "username password")
-            .then(user => {
-                if (!user) {
-                    //user not found
-                    return res.status(401).send({message: "Wrong username or password" });
-                }
-                //check the password
-                user.comparePassword(password, (err, isMatch) => {
-                    if (!isMatch) {
-                        //pasword does not match
-                        return res.status(401).send({ message: "Wrong username or password" });
-                    }
-                    //create a token
-                    const token = jwt.sign({ username: username }, process.env.SECRET);
-                    res.json({'token': token});
-
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    })
-
-
-}
-
+    // find this user name
+    User.findOne({ username }, 'username password')
+      .then((user) => {
+        if (!user) {
+          // user not found
+          return res.status(401).send({ message: 'Wrong username or password' });
+        }
+        // check the password
+        user.comparePassword(password, (err, isMatch) => {
+          if (!isMatch) {
+            // pasword does not match
+            return res.status(401).send({ message: 'Wrong username or password' });
+          }
+          // create a token
+          const token = jwt.sign({ username }, process.env.SECRET);
+          res.json({ token });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
--- api/controllers/textSourceController.js
+++ api/controllers/textSourceController.js
@@ -1,176 +1,98 @@
-const mongoose = require('mongoose');
-const TextSource = require('../models/textSourceModel');
 const jwt = require('jsonwebtoken');
-const multer = require("multer");
-const upload = multer({ dest: "uploads/"});
-const fs = require("fs")
-const axios = require("axios")
+const multer = require('multer');
 
-module.exports = app => {
+const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
+const axios = require('axios');
+const TextSource = require('../models/textSourceModel');
 
-    //ROOT ROUTE
-    app.get("/", (req, res) => {
-        res.json( {welcome: "Welcome to my Text Source API! Start by reading my documentation here https://ruhsane.github.io/Text_Source_Restful_API/#/ "})
-    })
-
-    //ALL TEXT FILES
-    app.get("/text_sources", checkToken, (req, res) => {
-        //verify the JWT token generated for the user
-        jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
-            if(err) {
-                console.log(err)
-                console.log('ERROR: cold not connect to the protected route');
-                res.sendStatus(403);
-            } else {
-                //if token is successfully verify, send the authorized data
-                TextSource.find({}, function(err, source) {
-                    if (err)
-                        res.json(err);
-                    res.json(source);
-                });
-                console.log('SUCCESS: connected to protected route');
-            }
-        })
-    });
-
-    //NEW TEXT SOURCE
-    app.post("/text_sources/new", checkToken, upload.single("content"), (req, res) => {
-        console.log(req.file)
-        //verify the JWT token generated for the user
-        jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
-            if(err) {
-                console.log('ERROR: cold not connect to the protected route');
-                res.sendStatus(403);
-            } else {
-                //if token is successfully verify, send the authorized data
-                var new_source = new TextSource(req.body);
-                const data = fs.readFileSync(req.file.path)
-                new_source.content = data
-                new_source.save(function(err, source) {
-                    if (err)
-                        res.json(err);
-                    res.json(source);
-                });
-                console.log('SUCCESS: connected to protected route');
-            }
-        })
-    });
-
-    //READ A SOURCE
-    app.get('/text_sources/:sourceId', checkToken, (req, res) => {
-
-        //verify the JWT token generated for the user
-        jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
-            if(err) {
-                console.log('ERROR: cold not connect to the protected route');
-                res.sendStatus(403);
-            } else {
-                //if token is successfully verify, send the authorized data
-                TextSource.findById(req.params.sourceId, function(err, source) {
-                    if (err)
-                      res.json(err);
-                    res.json(source);
-                });
-                console.log('SUCCESS: connected to protected route');
-            }
-        });
-
-    });
-
-     //UPDATE A SOURCE
-     app.put('/text_sources/:sourceId', checkToken, (req, res) => {
-
-        //verify the JWT token generated for the user
-        jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
-            if(err) {
-                console.log('ERROR: cold not connect to the protected route');
-                res.sendStatus(403);
-            } else {
-                //if token is successfully verify, send the authorized data
-        
-                TextSource.findOneAndUpdate({_id: req.params.sourceId}, req.body, {new: true}, function(err, source){
-                    if (err)
-                        res.json(err);
-                    res.json(source);
-            
-                });
-                console.log('SUCCESS: connected to protected route');
-            }
-        });
-
-    });
-
-
-
-
-    //DELETE A SOURCE
-    app.delete('/text_sources/:sourceId', checkToken, (req, res) => {
-
-        //verify the JWT token generated for the user
-        jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
-            if(err) {
-                console.log('ERROR: cold not connect to the protected route');
-                res.sendStatus(403);
-            } else {
-                //if token is successfully verify, send the authorized data
-            
-                TextSource.remove({
-                    _id: req.params.sourceId
-                }, function(err, source) {
-                    if (err)
-                        res.json(err);
-                    res.json({ message: "Text source successfully deleted"});                
-                });
-                console.log('SUCCESS: connected to protected route');
-            }
-        });
-    });
-
-    //GET MARKOV CHAIN API
-    app.get('/text_sources/:sourceId/get_markov', checkToken, (req, res) => {
-
-        //verify the JWT token generated for the user
-        jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
-            if(err) {
-                console.log('ERROR: cold not connect to the protected route');
-                res.sendStatus(403);
-            } else {
-                //Find the text source we are about to generate sentence on 
-                TextSource.findById(req.params.sourceId, function(err, source) {
-                    if (err)
-                      res.json(err);
-                    
-                    //Take in the source as the parameter to my text generator api
-                    axios.post('https://donald-trump-tweet-generator.herokuapp.com/api', source)
-                      .then(function (response) {
-                        //get the markov chain generated sentence
-                        res.json(response.data)
-                      })
-                      .catch(function (error) {
-                        res.json(error);
-                      });
-
-                });
-
-            }
-        })
-
-
-    })
-
-}  
-
-//check to make sure header is not undefined, if so return forbidden 403
+// check to make sure header is not undefined, if so return forbidden 403, and verify jwt token
 const checkToken = (req, res, next) => {
-    const header = req.headers['authorization'];
-    if(typeof header !== 'undefined') {
-        const bearer = header.split(' ');
-        const token = bearer[1];
+  const header = req.headers.authorization;
 
-        req.token = token;
-        next();
-    } else {
-        //if header is undefined
-        res.sendStatus(403)
-    }
-}
+  if (typeof header !== 'undefined') {
+    const bearer = header.split(' ');
+    const token = bearer[1];
+
+    req.token = token;
+    // VERIFY JWT TOKEN
+    jwt.verify(req.token, process.env.SECRET, (err, authorizedData) => {
+      if (err) return res.json(err);
+      req.user = authorizedData;
+      next();
+    });
+  } else {
+    // if header is undefined
+    res.sendStatus(403);
+  }
+};
+
+
+module.exports = (app) => {
+  // ROOT ROUTE
+  app.get('/', (req, res) => {
+    res.json({ welcome: 'Welcome to my Text Source API! Start by reading my documentation here https://ruhsane.github.io/Text_Source_Restful_API/#/ ' });
+  });
+
+  // ALL TEXT FILES
+  app.get('/text_sources', checkToken, (req, res) => {
+    TextSource.find({}, (err, source) => {
+      if (err) return res.json(err);
+      res.json(source);
+    });
+  });
+
+  // NEW TEXT SOURCE
+  app.post('/text_sources/new', checkToken, upload.single('content'), (req, res) => {
+    const new_source = new TextSource(req.body);
+    const data = fs.readFileSync(req.file.path);
+    new_source.content = data;
+    new_source.save((err, source) => {
+      if (err) return res.json(err);
+      res.json(source);
+    });
+  });
+
+  // READ A SOURCE
+  app.get('/text_sources/:sourceId', checkToken, (req, res) => {
+    TextSource.findById(req.params.sourceId, (err, source) => {
+      if (err) return res.json(err);
+      res.json(source);
+    });
+  });
+
+  // UPDATE A SOURCE
+  app.put('/text_sources/:sourceId', checkToken, (req, res) => {
+    TextSource.findOneAndUpdate({ _id: req.params.sourceId }, req.body, { new: true }, (err, source) => {
+      if (err) return res.json(err);
+      res.json(source);
+    });
+  });
+
+
+  // DELETE A SOURCE
+  app.delete('/text_sources/:sourceId', checkToken, (req, res) => {
+    TextSource.remove({ _id: req.params.sourceId }, (err, source) => {
+      if (err) return res.json(err);
+      res.json({ message: 'Text source successfully deleted', source });
+    });
+  });
+
+  // GET MARKOV CHAIN API
+  app.get('/text_sources/:sourceId/get_markov', checkToken, (req, res) => {
+    // Find the text source we are about to generate sentence on
+    TextSource.findById(req.params.sourceId, (err, source) => {
+      if (err) return res.json(err);
+
+      // Take in the source as the parameter to my text generator api
+      axios.post('https://donald-trump-tweet-generator.herokuapp.com/api', source)
+        .then((response) => {
+          // get the markov chain generated sentence and send it
+          res.json(response.data);
+        })
+        .catch((error) => {
+          res.json(error);
+        });
+    });
+  });
+};
